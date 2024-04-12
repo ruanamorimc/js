90291a401c004d5e2612fbe10b17fff14e4ce872
Ruan Amorim <allafruan27@gmail.com>
2024-04-12 18:33:09 -0300

    Estrutura de Repetição - while and do

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/* Essa estrutura é igual a de cima, porém, nessa primeiro ele testa e depois executa.
+
+ var c = 1
+ while (c <= 6) {
+     console.log(`Passo ${c}`)
+ }
+/*
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
